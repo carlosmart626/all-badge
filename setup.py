af649ad34acd2fd267a6ef93401cd8f93a2a7b5a
Carlos Martinez <carlosmart626@gmail.com>
2018-03-11 11:24:43 -0500

    Update coverage text

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests_require = [
 ]
 
 setup(name='all_badge',
-      version='0.1.1',
+      version='0.1.2',
       description='Generate badges for Coverage.py, Git tags and custom.',
       author='Carlos Martinez',
       author_email='me@carlosmart.co',
